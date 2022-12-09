5208efb53690103e8e4cb38b652c50058694d019
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2022-12-10 02:58:18 +0300

    veriler apiden çekildi yansıtıldı

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,28 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { addMovies } from '../../features/movies/movieSlice'
 import MovieListing from '../MovieListing/MovieListing'
 
 const Home = () => {
+const dispatch=useDispatch()
+
+
 useEffect(()=>{
 const movieText="Harry"
 
 
 const getData=async()=>{
   try {
-    const data = await axios.get(
+    const {data} = await axios.get(
       `http://www.omdbapi.com/?apikey=f09c43b8&s=${movieText}`
     );
+     dispatch(addMovies(data));
     console.log(data)
   } catch (error) {
     console.log(error.msg);
   }
- 
+
 }
 //  getData()
 
